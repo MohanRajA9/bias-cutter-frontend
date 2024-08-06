dd22f03d6012d8b1dd9e7f71f451065ec0ffef8a
MohanRajA9 <akmohanraj555@gmail.com>
2024-08-06 20:02:11 +0530

    Modified CSS of headings (Minor changes)

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-// export const URL = "https://bias-cutter-backend.onrender.com" 
-export const URL = "http://localhost:4000" 
+export const URL = "https://bias-cutter-backend.onrender.com" 
+// export const URL = "http://localhost:4000" 
